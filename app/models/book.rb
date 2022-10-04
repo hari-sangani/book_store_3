5ac4b6169ada2ec134cbb33106c2752b566b03a3
Harikrushna Sangani <harikrushna.sangani@softices.com>
2022-10-04 17:48:10 +0530

    typo-mistake

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,8 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :carts
   has_many :wishlists
-  has_many :order_books
-  has_many :orders, through: :order_books
+  has_many :orders_books
+  has_many :orders, through: :orders_books
   has_and_belongs_to_many :authors
   has_and_belongs_to_many :categories
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_books
-  has_many :books, through: :order_books
+  has_many :orders_books
+  has_many :books, through: :orders_books
 
   validates :user_id, :date, :status, presence: true
 end
