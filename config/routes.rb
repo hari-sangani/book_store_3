f1dba7ecd79b4d74adafde3ce3371577e1148898
Harikrushna Sangani <harikrushna.sangani@softices.com>
2022-10-20 14:26:49 +0530

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,6 @@ Rails.application.routes.draw do
   resources :carts
   resources :orders
   resources :books
-  resources :publishers
-  resources :authors
-  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
