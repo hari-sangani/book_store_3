79b0fd665c5ac0db5496694b09181c759108a880
Harikrushna Sangani <harikrushna.sangani@softices.com>
2022-11-03 10:40:49 +0530

    update I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  devise_for :users
-  scope "(:locale)", locale: /en|de/ do
+  scope '(:locale)', locale: /en|de/ do
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
+    devise_for :users
+    # Defines the root path route ("/")
+    root 'pages#home'
     get 'pages/home'
     get 'pages/about'
     get 'pages/contact'
@@ -13,10 +15,6 @@ Rails.application.routes.draw do
     resources :orders
     resources :books
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'pages#home'
 
   # mount Sidekiq::Web in your Rails app
   mount Sidekiq::Web => '/sidekiq'
